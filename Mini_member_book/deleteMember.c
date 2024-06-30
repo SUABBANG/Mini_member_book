2b9964b7aff23788c1e77467866f2bd90af34849
unknown <suabbang@gmail.com>
2024-06-30 18:05:59 +0900

    check delete

--- Mini_member_book/deleteMember.c
+++ Mini_member_book/deleteMember.c
@@ -1,11 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "header.h"
+#include <stdio.h>
+#include <stdlib.h> // for system("pause");
 
 void deleteMember(const char* phoneToDelete) {
     FILE* fp;
     FILE* tempFp;
     Member member;
     int found = 0;
+    char confirm;
 
     fp = fopen(CSV_FILE, "r");
     if (fp == NULL) {
@@ -21,21 +24,27 @@ void deleteMember(const char* phoneToDelete) {
     }
 
     while (fscanf(fp, "%[^,],%[^,],%[^\n]\n", member.name, member.phone, member.email) == 3) {
-        
         if (strcmp(member.phone, phoneToDelete) == 0) {
             found = 1;
+            printf("다음 회원을 삭제하시겠습니까?\n");
+            printf("이름: %s\n", member.name);
+            printf("전화번호: %s\n", member.phone);
+            printf("이메일: %s\n", member.email);
+            printf("정말로 삭제하려면 'y'를 입력하세요 (삭제하지 않으려면 다른 키를 입력하세요): ");
+            scanf(" %c", &confirm);
+            if (confirm != 'y' && confirm != 'Y') {
+                fprintf(tempFp, "%s,%s,%s\n", member.name, member.phone, member.email);
+            }
             continue; 
         }
-
 
         fprintf(tempFp, "%s,%s,%s\n", member.name, member.phone, member.email);
     }
 
-
     fclose(fp);
     fclose(tempFp);
 
-    // Remove the original file and rename the temporary file
+   
     remove(CSV_FILE);
     rename("temp.csv", CSV_FILE);
 
@@ -45,4 +54,6 @@ void deleteMember(const char* phoneToDelete) {
     else {
         printf("해당 전화번호를 가진 회원을 찾을 수 없습니다.\n");
     }
+
+    system("pause"); // 일시정지
 }
